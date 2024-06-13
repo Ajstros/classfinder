e15743aedc300776036d1e56241b13c371dbb387
Ajstros <ajstros@gmail.com>
2024-06-12 23:05:03 -0500

    Rename get_major_classes to read_major_classes. Add get_major_classes to pull from ClassFinder and then select major classes

--- classfinder.py
+++ classfinder.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 
-OUTPUT_FILE = "classes.csv"
+DEFAULT_OUTPUT_PATH = "classes.csv"
+DEFAULT_MAJOR_CLASSES_PATH = "major_classes.csv"
+DEFAULT_TAKEN_CLASSES_PATH = "taken_classes.csv"
 
 def term_str_to_code(term_string):
     """Convert term string to code for URL.
@@ -69,14 +71,14 @@ def get_classes(year, term, subjects):
             json_data = json.loads(elem.script.text, strict=False)
             description = json_data['description'].strip()
             class_dict["Course"].append(course)
-            class_dict["Section"].append(course)
+            class_dict["Section"].append(course_section)
             class_dict["Title"].append(title)
             class_dict["Description"].append(description)
     classes_df = pd.DataFrame(class_dict)
     return classes_df
 
-def get_major_classes(major_classes_file_path):
-    """Get the classes that apply to a major from a CSV list in the file path given.
+def read_major_classes(major_classes_file_path=DEFAULT_MAJOR_CLASSES_PATH):
+    """Read the classes that apply to a major from a CSV list in the file path given.
 
     Parameters
     ----------
@@ -93,7 +95,7 @@ def get_major_classes(major_classes_file_path):
         lines = f.readline()
     return lines.strip().split(',')
 
-def select_major_classes(classes_df, major_classes_file_path):
+def select_major_classes(classes_df, major_classes_file_path=DEFAULT_MAJOR_CLASSES_PATH):
     """Select only the classes that apply to a major from a DataFrame of classes.
 
     Parameters
@@ -106,15 +108,33 @@ def select_major_classes(classes_df, major_classes_file_path):
     Returns
     -------
     major_classes_only_df : pd.DataFrame
+        DataFrame of classes that apply to a major from the given classes including the Course number, the class Title, and its Description. Note this DataFrame is copied out of the given DataFrame.
+    """
+
+    major_classes = read_major_classes(major_classes_file_path)
+    return classes_df[classes_df["Course"].isin(major_classes)].copy()
+
+def get_major_classes(year, term, subjects, major_classes_file_path=DEFAULT_MAJOR_CLASSES_PATH):
+    """Get only the classes that apply to a major from the classfinder site.
+
+    Parameters
+    ----------
+    major_classes_file_path : str
+        Path to the CSV containing a list of major classes. E.g. "ETLS 676, ETLS 679".
+
+    Returns
+    -------
+    major_classes_only_df : pd.DataFrame
         DataFrame of classes that apply to a major from the given classes including the Course number, the class Title, and its Description.
     """
 
-    major_classes = get_major_classes(major_classes_file_path)
-    return df[df["Course"].isin(major_classes)]
+    full_df = get_classes(year=year, term=term, subjects=subjects)
+    return select_major_classes(full_df, major_classes_file_path)
 
 if __name__ == "__main__":
     year = 2024
     term = 'fall'
     subjects = ["ETLS", "SEIS"]
-    df = get_classes(year, term, subjects)
-    df.to_csv(OUTPUT_FILE)
+    major_df = get_major_classes(year, term, subjects)
+    major_df.to_csv(DEFAULT_OUTPUT_PATH)
+    print(major_df)
