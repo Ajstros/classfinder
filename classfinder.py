ad0f9e44a1085a6b1077e945f71a65123bdcf478
Ajstros <ajstros@gmail.com>
2024-06-19 23:47:08 -0500

    Check and format with ruff

--- classfinder.py
+++ classfinder.py
@@ -1,15 +1,17 @@
 """Pull data from classfinder and filter out to see what relevant classes are offered next semester."""
 
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import argparse
 from datetime import datetime
 from io import StringIO
 
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
 DEFAULT_OUTPUT_PATH = "classes.csv"
 DEFAULT_MAJOR_CLASSES_PATH = "major_classes.csv"
 DEFAULT_TAKEN_CLASSES_PATH = "taken_classes.csv"
+
 
 def get_year_term(day: datetime) -> tuple[int, str]:
     """Get the year and term for a specific day.
@@ -35,15 +37,16 @@ def get_year_term(day: datetime) -> tuple[int, str]:
     year = day.year
     month = day.month
     if month == 1:
-        term = 'j-term'
+        term = "j-term"
     elif month <= 5:
-        term = 'spring'
+        term = "spring"
     elif month <= 8:
-        term = 'summer'
+        term = "summer"
     else:
-        term = 'fall'
+        term = "fall"
 
     return year, term
+
 
 def term_str_to_code(term_string: str) -> int:
     """Convert term string to code for URL.
@@ -60,12 +63,13 @@ def term_str_to_code(term_string: str) -> int:
     """
 
     term_dict = {
-        'fall' : 40,
-        'summer' : 30,
-        'spring' : 20,
-        'j-term' : 10,
+        "fall": 40,
+        "summer": 30,
+        "spring": 20,
+        "j-term": 10,
     }
     return term_dict[term_string.lower()]
+
 
 def get_classes(year: int, term: str, subjects: list[str]) -> pd.DataFrame:
     """Get the classes for a specific year and term.
@@ -88,24 +92,26 @@ def get_classes(year: int, term: str, subjects: list[str]) -> pd.DataFrame:
     term_code = term_str_to_code(term)
     url = f"https://classes.aws.stthomas.edu/index.htm?year={year}&term={term_code}&schoolCode=ALL&levelCode=ALL&selectedSubjects={','.join(subjects)}#advancedSearch"
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
 
     subject_course_elems = soup.find_all("div", class_=["course", "cf"])
 
     class_dict = {"Course": [], "Section": [], "Title": [], "Description": []}
-    current_subject = ''
+    current_subject = ""
 
-    for elem in subject_course_elems :
-        if 'cf' in elem['class']:
+    for elem in subject_course_elems:
+        if "cf" in elem["class"]:
             # Subject
-            current_subject = elem.text.strip().split(':')[0]
+            current_subject = elem.text.strip().split(":")[0]
         else:
             # Course
-            course_number, course_section = elem.span.text.split('-')
-            course = f'{current_subject} {course_number}'
+            course_number, course_section = elem.span.text.split("-")
+            course = f"{current_subject} {course_number}"
             title = elem.strong.text
             json_data = pd.read_json(StringIO(elem.script.text))
-            description = json_data.drop_duplicates('name')['description'].iat[0].strip()
+            description = (
+                json_data.drop_duplicates("name")["description"].iat[0].strip()
+            )
             class_dict["Course"].append(course)
             class_dict["Section"].append(course_section)
             class_dict["Title"].append(title)
@@ -113,7 +119,10 @@ def get_classes(year: int, term: str, subjects: list[str]) -> pd.DataFrame:
     classes_df = pd.DataFrame(class_dict)
     return classes_df
 
-def read_major_classes(major_classes_file_path: str=DEFAULT_MAJOR_CLASSES_PATH) -> list[str]:
+
+def read_major_classes(
+    major_classes_file_path: str = DEFAULT_MAJOR_CLASSES_PATH,
+) -> list[str]:
     """Read the classes that apply to a major from a CSV list in the file path given.
 
     Parameters
@@ -127,11 +136,14 @@ def read_major_classes(major_classes_file_path: str=DEFAULT_MAJOR_CLASSES_PATH) 
         A list of the major classes from the CSV. E.g. ["ETLS 676", "ETLS 679"].
     """
 
-    with open(major_classes_file_path, 'r') as f:
+    with open(major_classes_file_path, "r") as f:
         lines = f.readline()
-    return lines.strip().split(',')
+    return lines.strip().split(",")
 
-def read_taken_classes(taken_classes_file_path: str=DEFAULT_TAKEN_CLASSES_PATH) -> list[str]:
+
+def read_taken_classes(
+    taken_classes_file_path: str = DEFAULT_TAKEN_CLASSES_PATH,
+) -> list[str]:
     """Read the classes that you have taken from a CSV list in the file path given.
 
     Parameters
@@ -145,11 +157,14 @@ def read_taken_classes(taken_classes_file_path: str=DEFAULT_TAKEN_CLASSES_PATH) 
         A list of the taken classes from the CSV. E.g. ["ETLS 676", "ETLS 679"].
     """
 
-    with open(taken_classes_file_path, 'r') as f:
+    with open(taken_classes_file_path, "r") as f:
         lines = f.readline()
-    return lines.strip().split(',')
+    return lines.strip().split(",")
 
-def select_major_classes(classes_df: pd.DataFrame, major_classes_file_path: str=DEFAULT_MAJOR_CLASSES_PATH) -> pd.DataFrame:
+
+def select_major_classes(
+    classes_df: pd.DataFrame, major_classes_file_path: str = DEFAULT_MAJOR_CLASSES_PATH
+) -> pd.DataFrame:
     """Select only the classes that apply to a major from a DataFrame of classes.
 
     Parameters
@@ -168,7 +183,10 @@ def select_major_classes(classes_df: pd.DataFrame, major_classes_file_path: str=
     major_classes = read_major_classes(major_classes_file_path)
     return classes_df.loc[classes_df["Course"].isin(major_classes)].copy()
 
-def select_not_taken_classes(classes_df: pd.DataFrame, taken_classes_file_path: str=DEFAULT_TAKEN_CLASSES_PATH) -> pd.DataFrame:
+
+def select_not_taken_classes(
+    classes_df: pd.DataFrame, taken_classes_file_path: str = DEFAULT_TAKEN_CLASSES_PATH
+) -> pd.DataFrame:
     """Select only the classes that have not been taken from a DataFrame of classes.
 
     Parameters
@@ -187,7 +205,13 @@ def select_not_taken_classes(classes_df: pd.DataFrame, taken_classes_file_path: 
     taken_classes = read_taken_classes(taken_classes_file_path)
     return classes_df.loc[~classes_df["Course"].isin(taken_classes)].copy()
 
-def get_major_classes(year: int, term: str, subjects: list[str], major_classes_file_path: str=DEFAULT_MAJOR_CLASSES_PATH) -> pd.DataFrame:
+
+def get_major_classes(
+    year: int,
+    term: str,
+    subjects: list[str],
+    major_classes_file_path: str = DEFAULT_MAJOR_CLASSES_PATH,
+) -> pd.DataFrame:
     """Get only the classes that apply to a major from the classfinder site.
 
     Parameters
@@ -216,17 +240,56 @@ DEFAULT_SUBJECTS = ["ETLS", "SEIS"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                        prog='ClassFinder',
-                        description='Pull data from classfinder and filter out to see what relevant classes are offered next semester.',
+        prog="ClassFinder",
+        description="Pull data from classfinder and filter out to see what relevant classes are offered next semester.",
     )
-    parser.add_argument('-o', '--output', default=DEFAULT_OUTPUT_PATH, help='output CSV file to write DataFrame to')
-    parser.add_argument('-y', '--year', default=DEFAULT_YEAR, help='year to find classes for')
-    parser.add_argument('-t', '--term', choices=['fall', 'summer', 'spring', 'j-term'], default=DEFAULT_TERM, help='year to find classes for')
-    parser.add_argument('-s', '--subjects', default=DEFAULT_SUBJECTS, help='subjects to find classes from')
-    parser.add_argument('-q', '--quiet', action='store_true', help='suppress all printing. Still outputs to a CSV')
-    parser.add_argument('-m', '--major', nargs='?', const=DEFAULT_MAJOR_CLASSES_PATH, help='find only classes that apply to a major. Uses the given CSV filename to read major classes')
-    parser.add_argument('-f', '--filter-taken', nargs='?', const=DEFAULT_TAKEN_CLASSES_PATH, help='find only classes not yet taken. Uses the given CSV filename to read taken classes')
-    parser.add_argument('--no-sections', action='store_true', help='do not include the sections column in the CSV or the printed output. Courses with multiple sections will be listed only once')
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=DEFAULT_OUTPUT_PATH,
+        help="output CSV file to write DataFrame to",
+    )
+    parser.add_argument(
+        "-y", "--year", default=DEFAULT_YEAR, help="year to find classes for"
+    )
+    parser.add_argument(
+        "-t",
+        "--term",
+        choices=["fall", "summer", "spring", "j-term"],
+        default=DEFAULT_TERM,
+        help="year to find classes for",
+    )
+    parser.add_argument(
+        "-s",
+        "--subjects",
+        default=DEFAULT_SUBJECTS,
+        help="subjects to find classes from",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="suppress all printing. Still outputs to a CSV",
+    )
+    parser.add_argument(
+        "-m",
+        "--major",
+        nargs="?",
+        const=DEFAULT_MAJOR_CLASSES_PATH,
+        help="find only classes that apply to a major. Uses the given CSV filename to read major classes",
+    )
+    parser.add_argument(
+        "-f",
+        "--filter-taken",
+        nargs="?",
+        const=DEFAULT_TAKEN_CLASSES_PATH,
+        help="find only classes not yet taken. Uses the given CSV filename to read taken classes",
+    )
+    parser.add_argument(
+        "--no-sections",
+        action="store_true",
+        help="do not include the sections column in the CSV or the printed output. Courses with multiple sections will be listed only once",
+    )
 
     args = parser.parse_args()
     year = args.year
@@ -235,22 +298,26 @@ if __name__ == "__main__":
 
     if args.major is not None:
         if not args.quiet:
-            print(f'Showing only major classes. Reading major classes from {args.major}')
+            print(
+                f"Showing only major classes. Reading major classes from {args.major}"
+            )
         df = get_major_classes(year, term, subjects, args.major)
     else:
         df = get_classes(year, term, subjects)
 
     if args.filter_taken is not None:
         if not args.quiet:
-            print(f'Showing only non-taken classes. Reading taken classes from {args.filter_taken}')
+            print(
+                f"Showing only non-taken classes. Reading taken classes from {args.filter_taken}"
+            )
         df = select_not_taken_classes(df, args.filter_taken)
 
     if args.no_sections:
-        df.drop(columns='Section', inplace=True)
+        df.drop(columns="Section", inplace=True)
         df.drop_duplicates(inplace=True)
 
     df.to_csv(args.output)
 
     if not args.quiet:
-        print(f'{term.title()} {year}\n')
+        print(f"{term.title()} {year}\n")
         print(df)
--- test_classfinder.py
+++ test_classfinder.py
@@ -1,44 +1,54 @@
 """Tests for classfinder script."""
 
+import random
 from datetime import datetime, timedelta
 
 import pytest
 
 import classfinder
-import random
 
 month_term_dict = {
-    1 : 'j-term',
-    2 : 'spring',
-    3 : 'spring',
-    4 : 'spring',
-    5 : 'spring',
-    6 : 'summer',
-    7 : 'summer',
-    8 : 'summer',
-    9 : 'fall',
-    10 : 'fall',
-    11 : 'fall',
-    12 : 'fall',
+    1: "j-term",
+    2: "spring",
+    3: "spring",
+    4: "spring",
+    5: "spring",
+    6: "summer",
+    7: "summer",
+    8: "summer",
+    9: "fall",
+    10: "fall",
+    11: "fall",
+    12: "fall",
 }
+
 
 def test_get_year_term():
     days_prev = 4 * 365
     days_next = 4 * 365
     today = datetime.today()
-    date_range = [today - timedelta(days=x) for x in range(days_prev)] + [today + timedelta(days=x) for x in range(days_next)]
+    date_range = [today - timedelta(days=x) for x in range(days_prev)] + [
+        today + timedelta(days=x) for x in range(days_next)
+    ]
     for day in date_range:
         assert classfinder.get_year_term(day) == (day.year, month_term_dict[day.month])
 
-@pytest.mark.parametrize('term_str', [
-    'fall', 'summer', 'spring', 'j-term',
-])
+
+@pytest.mark.parametrize(
+    "term_str",
+    [
+        "fall",
+        "summer",
+        "spring",
+        "j-term",
+    ],
+)
 def test_term_str_to_code(term_str):
     term_dict = {
-        'fall' : 40,
-        'summer' : 30,
-        'spring' : 20,
-        'j-term' : 10,
+        "fall": 40,
+        "summer": 30,
+        "spring": 20,
+        "j-term": 10,
     }
     term_code = term_dict[term_str]
 
@@ -48,31 +58,38 @@ def test_term_str_to_code(term_str):
     for n in range(num_caps):
         char_pos = random.choice(remaining_chars)
         remaining_chars.remove(char_pos)
-        term_str = term_str[0:char_pos] + term_str[char_pos].upper() + term_str[char_pos+1:]
+        term_str = (
+            term_str[0:char_pos] + term_str[char_pos].upper() + term_str[char_pos + 1 :]
+        )
 
     assert classfinder.term_str_to_code(term_str) == term_code
+
 
 @pytest.mark.skip()
 def test_get_classes():
     assert False
 
+
 @pytest.mark.skip()
 def test_read_major_classes():
     assert False
+
 
 @pytest.mark.skip()
 def test_read_taken_classes():
     assert False
 
+
 @pytest.mark.skip()
 def test_select_major_classes():
     assert False
+
 
 @pytest.mark.skip()
 def test_select_not_taken_classes():
     assert False
 
+
 @pytest.mark.skip()
 def test_get_major_classes():
     assert False
-
